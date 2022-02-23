e87a041833ed30712e31ad74ecd1b82ccb3710d6
hanji <o-range@kakao.com>
2022-02-23 17:56:31 +0900

    40개로 변경

--- src/service/state/reservation/atom.js
+++ src/service/state/reservation/atom.js
@@ -16,7 +16,7 @@ import {
 //객실가격의 더미데이터를 저장하는 atom
 export const roomTypeListAtom = atom({
   key: ROOMTYPE_LIST_ATOM,
-  default: makeDummyData(10),
+  default: makeDummyData(20),
 })
 
 //예약자정보의 더미데이터를 저장하는 atom
@@ -24,11 +24,11 @@ const fromToday = (i) => formatyyyyMMdd(addDays(i)(new Date()))
 export const reservationListAtom = atom({
   key: RESERVATION_LIST_ATOM,
   default: [
-    { checkIn: fromToday(6), checkOut: fromToday(7), color: '#34C38F', data: '김길동', location: '102호' },
-    { checkIn: fromToday(1), checkOut: fromToday(3), color: '#5B73E8', data: '한길동', location: '102호' },
-    { checkIn: fromToday(8), checkOut: fromToday(9), color: '#F46A6A', data: '박길동', location: '101호' },
-    { checkIn: fromToday(41), checkOut: fromToday(42), color: '#F1B44C', data: '송길동', location: '102호' },
-    { checkIn: fromToday(51), checkOut: fromToday(55), color: '#50A5F1', data: '하길동', location: '101호' },
+    { checkIn: fromToday(6), checkOut: fromToday(7), color: '#34C38F', data: '김길동', location: '2호' },
+    { checkIn: fromToday(1), checkOut: fromToday(3), color: '#5B73E8', data: '한길동', location: '2호' },
+    { checkIn: fromToday(8), checkOut: fromToday(9), color: '#F46A6A', data: '박길동', location: '1호' },
+    { checkIn: fromToday(41), checkOut: fromToday(42), color: '#F1B44C', data: '송길동', location: '2호' },
+    { checkIn: fromToday(51), checkOut: fromToday(55), color: '#50A5F1', data: '하길동', location: '1호' },
   ],
 })
 
@@ -36,12 +36,12 @@ export const reservationListAtom = atom({
 export const lockedRoomListAtom = atom({
   key: LOCKED_ROOM_LIST_ATOM,
   default: [
-    { targetDate: fromToday(20), location: '101호' },
-    { targetDate: fromToday(21), location: '101호' },
-    { targetDate: fromToday(22), location: '101호' },
-    { targetDate: fromToday(20), location: '102호' },
-    { targetDate: fromToday(21), location: '102호' },
-    { targetDate: fromToday(22), location: '102호' },
+    { targetDate: fromToday(20), location: '1호' },
+    { targetDate: fromToday(21), location: '1호' },
+    { targetDate: fromToday(22), location: '1호' },
+    { targetDate: fromToday(20), location: '2호' },
+    { targetDate: fromToday(21), location: '2호' },
+    { targetDate: fromToday(22), location: '2호' },
   ],
 })
 //Price컴포넌트에 예약이 있을때, 현재예약을 표시해주는 atom
