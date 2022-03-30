8c89924964dde5967cb12c047a754f1d9e8ff31c
abhidhamma-java <o-range@kakao.com>
2022-03-30 13:59:47 +0900

    회원관리 totalCount 페이지 수정

--- src/view/components/other/SignUp/CompanyManagement/List/List.js
+++ src/view/components/other/SignUp/CompanyManagement/List/List.js
@@ -1,7 +1,11 @@
+import { currentPageAtom, totalCountAtom } from '@state/common/paging'
 import { Suspense } from 'react'
+import { useRecoilValue } from 'recoil'
 import CompanyTable from './Table'
 
 export default function CompanyList() {
+  const totalCount = useRecoilValue(totalCountAtom)
+  const currentPage = useRecoilValue(currentPageAtom)
   return (
     <>
       {/* <!-- S:Container --> */}
@@ -12,7 +16,9 @@ export default function CompanyList() {
             <h3>회원관리</h3>
           </div>
           <div className='searchWrap'>
-            <span className='num'>Total 3,768건 1 페이지</span>
+            <span className='num'>
+              Total {totalCount}건 {currentPage}페이지
+            </span>
             <div className='searchBox'>
               <select className='mgr_5'>
                 <option>업체아이디</option>
