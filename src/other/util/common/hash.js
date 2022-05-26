de96e7c1d248e9813018661ad9125008f9ddc692
ahbidhamma-java <o-range@kakao.com>
2022-05-26 15:39:14 +0900

    encode decode 모듈 변경

--- src/other/util/common/hash.js
+++ src/other/util/common/hash.js
@@ -1,21 +1,24 @@
+import AES from 'crypto-js/aes'
+import cryptoJs from 'crypto-js'
+const privateKey = process.env.REACT_APP_ENCODE_KEY
 export const encode = (password) => {
-  const encodeArr = []
+  // const encodeArr = []
 
-  for (let c of password) {
-    c = c.charCodeAt()
-    encodeArr.push(c)
-  }
-  encodeArr.reverse()
-  return encodeArr
+  // for (let c of password) {
+  //   c = c.charCodeAt()
+  //   encodeArr.push(c)
+  // }
+  // encodeArr.reverse()
+  return AES.encrypt(password, privateKey).toString()
 }
 
 export const decode = (encodedPassword) => {
-  encodedPassword.reverse()
-  let password = ''
+  // encodedPassword.reverse()
+  // let password = ''
 
-  for (let c of encodedPassword) {
-    c = String.fromCharCode(c)
-    password += c
-  }
-  return password
+  // for (let c of encodedPassword) {
+  //   c = String.fromCharCode(c)
+  //   password += c
+  // }
+  return AES.decrypt(encodedPassword, privateKey).toString(cryptoJs.enc.Utf8)
 }
